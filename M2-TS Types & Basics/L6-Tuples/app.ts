c3457d1d1432d205adce17ca144cff8f9f36471f
ZairesDev <business.faissal@outlook.com>
2022-08-01 20:04:29 -0500

    finishied tuples

--- M2-TS Types & Basics/L6-Tuples/app.ts
+++ M2-TS Types & Basics/L6-Tuples/app.ts
@@ -1,13 +1,23 @@
-const myGuy = { 
+const myGuy: { 
+  name: string; 
+  age: number; 
+  hobbies: string[]; 
+  role: [number, string] 
+} = { 
   name: 'Faissal',
   age: 28,
-  hobbies: ['Sports', 'hanging']
+  hobbies: ['Sports', 'hanging'],
+  role: [3, 'athlete']
 };
+
+myGuy.role.push('teacher');
+// myGuy.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ['Apex']
 
 console.log(myGuy.name);
+console.log(myGuy.role);
 
 for(const hobby of myGuy.hobbies) {
   console.log(hobby.toLocaleUpperCase());
