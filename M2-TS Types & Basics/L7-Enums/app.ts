4228767db846eb7c3e832511b22bf05942dc0941
ZairesDev <business.faissal@outlook.com>
2022-08-01 20:27:31 -0500

    finished enums

--- M2-TS Types & Basics/L7-Enums/app.ts
+++ M2-TS Types & Basics/L7-Enums/app.ts
@@ -1,25 +1,42 @@
-const myGuy: { 
-  name: string; 
-  age: number; 
-  hobbies: string[]; 
-  role: [number, string] 
-} = { 
+// const myGuy: { 
+//   name: string; 
+//   age: number; 
+//   hobbies: string[]; 
+//   role: [number, string] 
+// } = { 
+//   name: 'Faissal',
+//   age: 28,
+//   hobbies: ['Sports', 'hanging'],
+//   role: [3, 'athlete']
+// };
+
+// myGuy.role.push('teacher');
+// myGuy.role[1] = 10;
+
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const myGuy = { 
   name: 'Faissal',
   age: 28,
   hobbies: ['Sports', 'hanging'],
-  role: [3, 'athlete']
+  role: Role.ADMIN
 };
-
-myGuy.role.push('teacher');
-// myGuy.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ['Apex']
 
-console.log(myGuy.name);
-console.log(myGuy.role);
-
-for(const hobby of myGuy.hobbies) {
-  console.log(hobby.toLocaleUpperCase());
-  // console.log(hobby.map()); // ERROR gives an error due to individual hobbies having a string data type.
+if(myGuy.role === Role.ADMIN) {
+  console.log('is Admin')
 }
+// console.log(myGuy.name);
+// console.log(myGuy.role);
+
+// for(const hobby of myGuy.hobbies) {
+//   console.log(hobby.toLocaleUpperCase());
+//   // console.log(hobby.map()); // ERROR gives an error due to individual hobbies having a string data type.
+// }
