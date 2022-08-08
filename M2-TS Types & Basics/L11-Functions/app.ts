00d5c191935cd2e59860f18dfc1d71fed0acfec6
ZairesDev <business.faissal@outlook.com>
2022-08-08 12:37:07 -0500

    intro to undefined

--- M2-TS Types & Basics/L11-Functions/app.ts
+++ M2-TS Types & Basics/L11-Functions/app.ts
@@ -2,8 +2,18 @@ function add(n1: number, n2: number) {
   return n1 + n2
 }
 
-function printResult(num: number) {
+// has no return statement, or return anything. Therefore void.
+// 
+function printResult(num: number): void {
   console.log('Result: ' + num)
 }
 
+// undefined is also a type in TS, however you will need a return statement that is empty
+// function printResult(num: number): undefined {
+//   console.log('Result: ' + num)
+//   return;
+// }
+
 printResult(add(6,12))
+
+let someValue: undefined;
