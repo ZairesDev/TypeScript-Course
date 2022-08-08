982b4f28e4a7deb9d47bdc1c3e8143ff1b5e2185
ZairesDev <business.faissal@outlook.com>
2022-08-08 10:57:10 -0500

    Literal Types completed

--- M2-TS Types & Basics/L9-Literal/app.ts
+++ M2-TS Types & Basics/L9-Literal/app.ts
@@ -1,16 +1,18 @@
-function combine(input1: number | string, input2: number | string, resultType: string) {
+function combine(input1: number | string, input2: number | string, resultType: 'as-number' | 'as-text') {
   // this type of run time type check may not always be needed with union types.
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+    result = +input1 + +input2
   } else {
     result = input1.toString() + input2.toString()
   }
-  if (resultType === 'as-number'){
-    // +result, with the plus right behind the result will convert the result to a number
-    return +result
-  }
   return result;
+  // if (resultType === 'as-number'){
+  //   // +result, with the plus right behind the result will convert the result to a number. same as parseFloat(result);.
+  //   return +result
+  // } else {
+  //   return result.toString();
+  // }
 }
 
 const combineAges = combine(30,26, 'as-number');
