e3d6ae7264d24151b986c87b80702bdb4bb205bc
ZairesDev <business.faissal@outlook.com>
2022-08-12 16:17:00 -0500

    added unknown type

--- M2-TS Types & Basics/L11-Functions/app.ts
+++ M2-TS Types & Basics/L11-Functions/app.ts
@@ -1,19 +1,11 @@
-function add(n1: number, n2: number) {
-  return n1 + n2
+let userInput: unknown;
+let userName: string;
+
+
+userInput = 5; 
+userInput = 'Faissal'
+// this will throw an error since we can't assign a fixed type to an unknown type
+// userName = userInput;
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-// has no return statement, or return anything. Therefore void.
-// 
-function printResult(num: number): void {
-  console.log('Result: ' + num)
-}
-
-// undefined is also a type in TS, however you will need a return statement that is empty
-// function printResult(num: number): undefined {
-//   console.log('Result: ' + num)
-//   return;
-// }
-
-printResult(add(6,12))
-
-let someValue: undefined;
