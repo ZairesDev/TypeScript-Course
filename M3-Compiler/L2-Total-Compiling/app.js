e858c2e3c05e597d4a601dab2825f462f3e6a155
Faissal Shabani <faissal.shabani@ideaentity.com>
2022-09-14 13:44:58 -0500

    learned about configuring the tsconfig file

--- M3-Compiler/L2-Total-Compiling/app.js
+++ M3-Compiler/L2-Total-Compiling/app.js
@@ -1,3 +1,7 @@
 "use strict";
 const userName = 'Faissal';
 console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Hello World');
+});
--- M3-Compiler/L2-Total-Compiling/app.ts
+++ M3-Compiler/L2-Total-Compiling/app.ts
@@ -3,4 +3,8 @@ const userName = 'Faissal';
 console.log(userName)
 
 
+const button = document.querySelector('button')!;
 
+button.addEventListener('click', () => {
+  console.log('Hello World')
+})
