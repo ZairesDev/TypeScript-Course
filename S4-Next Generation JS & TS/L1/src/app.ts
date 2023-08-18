30182d4fe35036d0c56a3d34a98b408cd4f8fad8
Faissal Shabani <faissal.shabani@ideaentity.com>
2023-08-18 12:56:16 -0500

    lesson 2 arrow recap

--- S4-Next Generation JS & TS/L1/src/app.ts
+++ S4-Next Generation JS & TS/L1/src/app.ts
@@ -8,8 +8,8 @@ function add(a: number, b: number) {
   return result;
 }
 
-if (age > 20) {
-  let isOld = true;
-}
-// will throw an error because isOld is block scoped and the console.log is outside the block
-console.log(isOld);
+// if (age > 20) {
+//   let isOld = true;
+// }
+// // will throw an error because isOld is block scoped and the console.log is outside the block
+// console.log(isOld);
