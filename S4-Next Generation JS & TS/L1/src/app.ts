732791e39067188bd863e230e3524c0e171c1c2c
Faissal Shabani <faissal.shabani@ideaentity.com>
2023-08-18 11:34:15 -0500

    let and const review

--- S4-Next Generation JS & TS/L1/src/app.ts
+++ S4-Next Generation JS & TS/L1/src/app.ts
@@ -1,1 +1,15 @@
 // Code goes here!
+const userName = 'Faissa';
+let age = 25;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+if (age > 20) {
+  let isOld = true;
+}
+// will throw an error because isOld is block scoped and the console.log is outside the block
+console.log(isOld);
