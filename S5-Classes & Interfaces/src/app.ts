81341bfc992ecc14eea36fb8835e718617a65af3
Faissal Shabani <faissal.shabani@ideaentity.com>
2023-08-21 14:34:51 -0500

    public and private/access modifiers

--- S5-Classes & Interfaces/src/app.ts
+++ S5-Classes & Interfaces/src/app.ts
@@ -1,20 +1,37 @@
 class Department {
-  name: string;
+  // private id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(n:string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.name = n;
+    // this.id = id;
   };
 
 
   // this:Department tells TS that the 'this' will always refer to the Department class
   describe(this: Department) {
-    console.log('Department: ' + this.name)
+    console.log(`Department (${this.id}): ${this.name}`)
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 // the constructor argument will be gathered from the new instance
-const accounting = new Department('Accounting');
-accounting.describe();
+const accounting = new Department('45','Accounting');
 
-const accountingCopy = { name: 'happy', describe: accounting.describe };
-accountingCopy.describe();
+accounting.addEmployee('Godzilla');
+accounting.addEmployee('King Kong');
+
+// accounting.employees[2] = 'Mothra';
+
+accounting.describe();
+accounting.name = 'Stone Cold Steve Austin'
+accounting.printEmployeeInfo();
