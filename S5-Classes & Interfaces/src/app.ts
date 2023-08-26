4a3ad22afd7f1c4bc8edb378d30982b4d7666078
Faissal Shabani <faissal.shabani@ideaentity.com>
2023-08-26 10:37:13 -0500

    completed interfaces quiz

--- S5-Classes & Interfaces/src/app.ts
+++ S5-Classes & Interfaces/src/app.ts
@@ -1,5 +1,17 @@
+// type AddFunction = (a: number, b: number) => number;
+interface AddFunction {
+  (a: number, b: number): number;
+}
+
+let add: AddFunction;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+}
+
 interface Named {
-  readonly firstName: string;
+  readonly firstName?: string;
+  outputFirstName?: string;
 }
 
 interface Greetable extends Named {  
@@ -7,24 +19,30 @@ interface Greetable extends Named {
 }
 
 class Player implements Greetable {
-  firstName: string;
+  firstName?: string;
   age = 888;
 
 
 
-  constructor(n: string) {
-    this.firstName = n;
+  constructor(n?: string) {
+    if(n) {
+      this.firstName = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.firstName)
+    if(this.firstName) {
+      console.log(phrase + ' ' + this.firstName)
+    } else {
+      console.log('Hi')
+    }
   }
 }
 
 // setting it to type Greetable means that it must have the same properties as the Greetable interface
 let player1: Greetable;
 
-player1 = new Player('Obiwan');
+player1 = new Player();
 
 player1.greet('Hi There - I am');
 console.log(player1)
