7d599c3b5d80e52325a11308de607cffdca793d2
ZairesDev <business.faissal@outlook.com>
2023-12-11 14:05:29 -0600

    generic function

--- S7-Generics/src/app.ts
+++ S7-Generics/src/app.ts
@@ -1,2 +1,6 @@
-// Code goes here!
-const names: Array = [];
+function merge<T extends object , U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+const mergedObj = merge({name: 'Faissal', hobbies: ['sports'] }, {age: 28})
+console.log(mergedObj)
