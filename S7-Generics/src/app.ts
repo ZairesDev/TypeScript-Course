771208571e3803705e784bc1de201d5732d6dbf5
ZairesDev <business.faissal@outlook.com>
2024-01-12 13:01:42 -0600

    utilizing the keyof constraint in generics

--- S7-Generics/src/app.ts
+++ S7-Generics/src/app.ts
@@ -4,3 +4,27 @@ function merge<T extends object , U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({name: 'Faissal', hobbies: ['sports'] }, {age: 28})
 console.log(mergedObj)
+
+interface Lengthy {
+  length: number
+}
+
+const countAndDescribe = <T extends Lengthy>(element: T): [T, string] => {
+  let descriptionText = 'Luke, I am your father'
+  if (element.length === 1) {
+    descriptionText = 'Got one element'
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`
+  }
+  return [element, descriptionText]
+}
+
+// console.log(countAndDescribe('Hi there!'))
+// console.log(countAndDescribe(['Sports', 'Formula1']))
+// console.log(countAndDescribe(10))
+
+const extractAndConvert = <T extends object, U extends keyof T>(obj: T, key:U) => {
+  return obj[key]
+}
+
+console.log(extractAndConvert({name: 'Faissal'}, 'name'))
