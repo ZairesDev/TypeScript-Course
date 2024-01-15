dc74f4ddb292dac8d0cbb024bf9e3a40e543e817
ZairesDev <business.faissal@outlook.com>
2024-01-15 15:43:58 -0600

    generic utility types

--- S7-Generics/src/app.ts
+++ S7-Generics/src/app.ts
@@ -69,3 +69,28 @@ console.log(numberStorage.getItems())
 // objStorage.addItem({name: 'Darth Vader'})
 // objStorage.removeItem({name: 'Faissal'})
 // console.log(objStorage.getItems())
+
+// End Generic Classes
+
+// Generic Utility Types
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal (title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+  return courseGoal as CourseGoal
+}
+
+const names: Readonly<string[]> = ['Darth', 'Vader']
+// names.push('Faissal')
+
+// End Generic Utility Types
+
+
