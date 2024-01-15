748b6c4a79f7fffe00b616e4568a3f1821a077f0
ZairesDev <business.faissal@outlook.com>
2024-01-15 15:34:37 -0600

    generic classes

--- S7-Generics/src/app.ts
+++ S7-Generics/src/app.ts
@@ -1,3 +1,4 @@
+// Generic Functions
 function merge<T extends object , U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB)
 }
@@ -24,7 +25,47 @@ const countAndDescribe = <T extends Lengthy>(element: T): [T, string] => {
 // console.log(countAndDescribe(10))
 
 const extractAndConvert = <T extends object, U extends keyof T>(obj: T, key:U) => {
-  return obj[key]
+  return 'Value: ' + obj[key]
 }
 
 console.log(extractAndConvert({name: 'Faissal'}, 'name'))
+// End Generic Functions
+
+// Generic Classes
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(item:T ) {
+    if (this.data.indexOf(item) === -1) {
+      return
+    }
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('help')
+textStorage.addItem('me')
+textStorage.removeItem('help')
+console.log(textStorage.getItems())
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(10)
+numberStorage.addItem(20)
+numberStorage.removeItem(10)
+console.log(numberStorage.getItems())
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({name: 'Faissal'})
+// objStorage.addItem({name: 'Darth Vader'})
+// objStorage.removeItem({name: 'Faissal'})
+// console.log(objStorage.getItems())
