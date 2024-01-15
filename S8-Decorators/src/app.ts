cc4569ee1f1c21f135e5757d15fb7ef22e4c1feb
ZairesDev <business.faissal@outlook.com>
2024-01-15 17:36:03 -0600

    all of the places that you can use decorators

--- S8-Decorators/src/app.ts
+++ S8-Decorators/src/app.ts
@@ -18,12 +18,10 @@ function Logger(logString: string) {
 
 // Decorator Factory with Template
 function WithTemplate(template: string, hookId: string) {
-  return function(constructor: any) {
+  return function(_: Function) {
     const hookEl = document.getElementById(hookId)
-    const p = new constructor()
     if (hookEl) {
       hookEl.innerHTML = template
-      hookEl.querySelector('h1')!.textContent = p.name
     }
   }
 }
@@ -44,3 +42,59 @@ class FormulaOneTeam {
 const team = new FormulaOneTeam()
 
 console.log(team)
+
+// ---
+
+// Property Decorators
+// If the decorator is attached to a property, the constructor function of the class is passed as the second argument
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator!')
+  console.log(target, propertyName)
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Paramerter decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
+
+class Product {
+  @Log
+  title: string
+  private _price: number
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val
+    } else {
+      throw new Error('Invalid price - should be positive!')
+    }
+  }
+  
+  constructor(t: string, p: number) {
+    this.title = t
+    this._price = p
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax)
+  }
+}
